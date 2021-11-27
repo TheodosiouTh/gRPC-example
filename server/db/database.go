316b2d8ba7e9d243796ef4935925b2bc8b82f9ad
TheodosiouTh <thanosthd@gmail.com>
2021-11-27 17:12:10 +0200

    Implement reconnection mechanism to fix the DB intialization after server startup

--- server/db/database.go
+++ server/db/database.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,8 +13,8 @@ var db *gorm.DB
 var err error
 
 func Init(host, dbname, user, password string, port int) error {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable", host, user, password, dbname, port)
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable", host, user, password, dbname, port)
+	db, err = connectWithRetries(3, connectionString)
 	if err != nil {
 		return err
 	}
@@ -21,6 +22,21 @@ func Init(host, dbname, user, password string, port int) error {
 	log.Println("Connection to DB established")
 	migrateModels()
 	return nil
+}
+
+func connectWithRetries(retries int, connectionString string) (*gorm.DB, error) {
+	connection, er := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+	retries--
+	for er != nil || retries == 0 {
+		time.Sleep(5 * time.Second)
+		connection, er = gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+		retries--
+	}
+
+	if er != nil {
+		return nil, er
+	}
+	return connection, nil
 }
 
 func migrateModels() {
